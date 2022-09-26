b516598611983af677f5fbf0b49c679e4aa2c57b
Mike Kokko <mike.kokko@gmail.com>
2022-09-26 10:30:48 -0400

    got a little bit more working

--- polaris_windows/polaris_windows/Source.cpp
+++ polaris_windows/polaris_windows/Source.cpp
@@ -9,16 +9,42 @@ using namespace serial;
 int main(void){
 	
 	// open the serial port
+	cout << "Opening serial port..." << endl;
 	std::string myPort("COM5");
-	Serial mySerialPort(myPort, 9600U, Timeout(10,10,10,10,10), eightbits, parity_none, stopbits_one, flowcontrol_none);
-	
+	Serial*  mySerialPort = NULL;
+	try {
+		mySerialPort = new Serial(myPort, 9600U, Timeout(100, 100, 100, 100, 100), eightbits, parity_none, stopbits_one, flowcontrol_none);
+	}
+	catch (IOException e) {
+		cout << e.what();
+		return -1;
+	};
+	mySerialPort->flush();
+
+
 	// equiv of serial break... it works!
-	mySerialPort.setBreak(true);
+	cout << "Sending serial break..." << endl;
+	mySerialPort->setBreak(true);
 	Sleep(10);
-	mySerialPort.setBreak(false);
+	mySerialPort->setBreak(false);
+
+	// wait a bit, then read status
+	Sleep(1000);
+	string readData = mySerialPort->read(size_t(1000));
+	cout << "Read: " << readData << endl;
+
+	// wait a bit longer
+	// TODO: figure out why we need this...
+	Sleep(1000);
+
+	// send a beep command
+	cout << "Sending BEEP command..." << endl;
+	mySerialPort->write("BEEP 9\r");
+	readData = mySerialPort->read(size_t(1000));
+	cout << "Read: " << readData << endl;
 
 	// close the serial port
-	mySerialPort.close();
+	mySerialPort->close();
 
 	// report success
 	cout << "Done!" << endl;
